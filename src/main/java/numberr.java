e1202d22a34095c4ad351c439549bfd943fa9ac1
Antonina23 <tone4kawow@gmail.com>
2025-05-25 21:53:53 +0400

    initial commit

---
+++ src/main/java/numberr.java
@@ -0,0 +1,11 @@
+public class numberr {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int z;
+
+        z = x + y;
+
+        System.out.println(z);
+    }
+}
