34c736ad6ac7433975d1b86c668f835d3c8526e5
Antonina23 <tone4kawow@gmail.com>
2025-06-17 21:46:12 +0400

    Zoo2

--- src/main/java/test2.java
+++ src/main/java/test2.java
@@ -1,13 +1,15 @@
+import java.util.ArrayList;
+
 public class test2 {
     public static void main(String[] args) {
-        int x;
-        int y;
-        int z;
-        x=1;
-        y=2;
-        z = x + y;
+        ArrayList<Integer> lists = new ArrayList<>();
 
+        lists.add(1);
+        lists.add(2);
 
-        System.out.println(z);
+        System.out.println(lists);
     }
+
+
 }
+
