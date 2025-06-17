16c930bd31de574db46c517518df7d4281b84694
Antonina23 <tone4kawow@gmail.com>
2025-06-17 20:12:38 +0400

    initial commit

---
+++ src/main/java/test2.java
@@ -0,0 +1,13 @@
+public class test2 {
+    public static void main(String[] args) {
+        int x;
+        int y;
+        int z;
+        x=1;
+        y=2;
+        z = x + y;
+
+
+        System.out.println(z);
+    }
+}
